f28d1f7e5bba5ac71a4382eb7786b2547f369979
Chinmay Rawool <chinmay811@gmail.com>
2017-03-27 18:42:57 -0400

    Initial commit

---
+++ app/src/main/java/com/mad/chitchat/MainActivity.java
@@ -0,0 +1,19 @@
+package com.mad.chitchat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/*
+* Login and Sign up activity
+*
+* */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
